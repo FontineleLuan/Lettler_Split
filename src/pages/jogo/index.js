107975536afe7910c872ddc4b146fd53568da4c2
Luan Fontinele <luanfontinele18@gmail.com>
2026-01-21 21:40:32 -0300

    visual feito, iniciar logica

--- src/pages/jogo/index.js
+++ src/pages/jogo/index.js
@@ -1,17 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
 import {Text, View } from 'react-native';
+import { useState } from 'react';
 import { styles } from "./styles"
-import { FlatList } from 'react-native-web';
+import { FlatList, Image } from 'react-native';
+import { TextInput } from 'react-native';
 
 export default function jogo() {
+
+  const [grid,setGrid] = useState([
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+  ])
+  const [currentRow, setCurrentRow] = useState(0);
+  const [currentWord, setCurrentWord] = useState("");
+
+
+
   return (
     <View style={styles.container}>
         <View style={styles.header}>
+          <Image 
+            source={require('../../../assets/option.png')}
+            style={styles.option} />
           <Text style={styles.title}>
-              
+              Lettler <Text style={styles.titleDifferent}>Split</Text>
+          </Text>
+
+          <Image 
+            source={require('../../../assets/user.png')}
+            style={styles.perfil}
+          />
+        </View>
+
+        <View style={styles.subTitle}>
+          <Text style={styles.description}>
+            Adivinhe a palavra!!
           </Text>
         </View>
+        <View style={styles.keyboard}>
+          <TextInput
+                value={currentWord}
+                onChangeText={setCurrentWord}
+                maxLength={5}
+                style={styles.keyBoardHolder}
+              />
+        </View>
         <View style={styles.box}>
+          {grid.map((row, rowIndex) => (
+            <View
+              key={rowIndex}
+              style={styles.rowLine}
+            >
+            
+          {row.map((cell, colIndex) => (
+            <View
+              key={colIndex}
+              style={[
+                styles.rowCollunm,
+                rowIndex === currentRow && { backgroundColor: "#dddddd" }
+              ]}
+            >
+              <Text style={styles.placeHolder}>{(rowIndex === currentRow ? currentWord[colIndex] || "" : cell).toUpperCase()}</Text>
+            </View>
+          ))}
+          </View>
+          ))}
         </View>
     </View>
   );
--- src/pages/jogo/styles.js
+++ src/pages/jogo/styles.js
@@ -1,10 +1,99 @@
 import { StyleSheet } from "react-native";
 
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#82785a',
     alignItems: 'center',
-    justifyContent: 'center',
+
   },
+
+  header:{
+    width: "100%",
+    flexDirection: "row",
+    marginTop: "3%",
+    alignItems: "center",
+    padding: 5,
+    justifyContent: "space-between",
+  },
+
+  title: {
+    fontSize: 25,
+    color: "white",
+    fontFamily: 'monospace'
+  },
+
+  titleDifferent: {
+    color: "#edddb4",
+    fontSize: 27,
+  },
+
+  option: {
+    width: 40,
+    resizeMode: "contain"
+  },
+
+  perfil: {
+    width: 40,
+    resizeMode: "contain"
+  },
+
+  subTitle: {
+    alignItems: "center",
+    justifyContent: "center"
+  },
+
+  description: {
+    marginTop: "30%",
+    fontSize: 15,
+    color: "white",
+    fontFamily: 'monospace'
+  },
+
+  box: {
+    marginTop: "10%",
+    width: "100%",
+    padding: 35
+  },
+
+  rowLine:{
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  rowCollunm: {
+    width: 60,
+    height: 60,
+    borderRadius: "5%",
+    marginBottom: "4%",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#535044",
+  },
+
+  keyboard: {
+    width: "100%",
+    height: "10%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "20%",
+  },
+
+  keyBoardHolder: {
+    width: "80%",
+    height: "90%",
+    fontSize: 35,
+    textAlign: "center",
+    textAlignVertical: "center",
+    borderWidth: 1,
+    outlineStyle: "none",
+    borderColor: "white",
+    borderRadius: 10,
+    backgroundColor: "#a7a6a6"
+  },
+
+  placeHolder:{
+    fontSize: 30,
+  }
 });
